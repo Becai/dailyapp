4b59c3c486b942333fe3688499c7bf9f4ca58abd
becai <942645402@qq.com>
2022-01-07 15:51:00 +0800

    修改重复声明

--- src/main.js
+++ src/main.js
@@ -7,11 +7,6 @@ import VueAxios from 'vue-axios'
 
 Vue.use(VueAxios, axios)
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
-Vue.use(VueAxios, axios)
-
 Vue.config.productionTip = false
 
 new Vue({
