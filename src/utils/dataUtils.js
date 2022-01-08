15b910fc9b32366400b80b3fdfbfbd7327e0edc7
Hami Lemon <hamilemon@aliyun.com>
2022-01-08 18:40:50 +0800

    实现下拉加载

---
+++ src/utils/dataUtils.js
@@ -0,0 +1,93 @@
+/**
+ * 生成开发用的假数据
+ */
+let indexTemplet = {
+    "reason": "success",
+    "result": {
+        "stat": "1",
+        "data": [
+            {
+                "uniquekey": "db61b977d9fabd0429c6d0c671aeb30e",
+                "title": "“新时代女性的自我关爱”主题沙龙暨双山街道福泰社区妇儿活动家园启动仪式举行",
+                "date": "2021-03-08 13:47:00",
+                "category": "头条",
+                "author_name": "鲁网",
+                "url": "https://mini.eastday.com/mobile/210308134708834241845.html",
+                "thumbnail_pic_s": "https://dfzximg02.dftoutiao.com/news/20210308/20210308134708_d0216565f1d6fe1abdfa03efb4f3e23c_0_mwpm_03201609.png",
+                "thumbnail_pic_s02": "https://dfzximg02.dftoutiao.com/news/20210308/20210308134708_d0216565f1d6fe1abdfa03efb4f3e23c_1_mwpm_03201609.png",
+                "thumbnail_pic_s03": "https://dfzximg02.dftoutiao.com/news/20210308/20210308134708_d0216565f1d6fe1abdfa03efb4f3e23c_2_mwpm_03201609.png",
+                "is_content": "1"
+            },
+            {
+                "uniquekey": "7d246cbfa9000fb5ac42fb3bb934a592",
+                "title": "滴滴发布女司机数据：2020年新增女性网约车司机超26万",
+                "date": "2021-03-08 13:40:00",
+                "category": "头条",
+                "author_name": "国青年网",
+                "url": "https://mini.eastday.com/mobile/210308134023641877777.html",
+                "thumbnail_pic_s": "https://dfzximg02.dftoutiao.com/news/20210308/20210308134023_7a9ca0543b00332147c42e1ee4146908_0_mwpm_03201609.png",
+                "thumbnail_pic_s02": "https://dfzximg02.dftoutiao.com/news/20210308/20210308134023_7a9ca0543b00332147c42e1ee4146908_1_mwpm_03201609.jpeg",
+                "is_content": "1"
+            },
+            {
+                "uniquekey": "c0611bea6eb961a57b21a0d1008bbe2e",
+                "title": "点赞！东海县公安局学雷锋见行动",
+                "date": "2021-03-08 13:38:00",
+                "category": "头条",
+                "author_name": "江南时报",
+                "url": "https://mini.eastday.com/mobile/210308133849892734209.html",
+                "thumbnail_pic_s": "https://dfzximg02.dftoutiao.com/news/20210308/20210308133849_b9f3d069a1ab400bf2d87fcc15793ca5_1_mwpm_03201609.png",
+                "thumbnail_pic_s02": "https://dfzximg02.dftoutiao.com/news/20210308/20210308133849_b9f3d069a1ab400bf2d87fcc15793ca5_2_mwpm_03201609.png",
+                "thumbnail_pic_s03": "https://dfzximg02.dftoutiao.com/news/20210308/20210308133849_b9f3d069a1ab400bf2d87fcc15793ca5_3_mwpm_03201609.png",
+                "is_content": "1"
+            }
+        ],
+        "page": "1",
+        "pageSize": "3"
+    },
+    "error_code": 0
+}
+
+let contentTemplet = {
+    "reason": "查询成功!",
+    "result": {
+        "uniquekey": "b6007680102715c423da7ae88862ab7b",
+        "detail": {
+            "title": "黄子韬入盟GK，娱乐、电竞圈互串双丰收？",
+            "date": "2021-03-08 13:21:00",
+            "category": "娱乐",
+            "author_name": "杨啸宇",
+            "url": "https://mini.eastday.com/mobile/210308132121345627278.html",
+            "thumbnail_pic_s": "https://dfzximg02.dftoutiao.com/minimodify/20210308/500x700_6045b450aafd7_mwpm_03201609.png",
+            "thumbnail_pic_s02": "",
+            "thumbnail_pic_s03": ""
+        },
+        "content": "<p >其实明星当中有很对人玩网络游戏，特别是英雄联盟和王者荣耀，最近佛山GK电子竞技俱乐部宣布，黄子韬将加盟GK，担任合伙人兼联席CEO。今后，他将与俱乐部合作培养选手和艺人。</p><p >\n            <img width='100%' src='//dfzximg01.dftoutiao.com/minimodify/20210308/500x700_6045b42b1dd23.png' data-weight='500' data-width='500' data-height='700'></p><p >娱乐产业与游戏产业之间虽然存在壁垒，但壁垒并不深，明星与电子竞技圈的跨界现象屡见不鲜。杨幂、罗云熙、张彬彬、陈赫等“资深游戏玩家”经常出现在《英雄联盟》和《王者荣耀》之间的表演比赛、评论席和自制综艺节目中。近年来，明星跨界电子竞技的形式不超过两种。一是与正式比赛和电子竞技项目进行互动合作，二是组建像周杰伦、余文乐、林俊杰等自己的团队。但现在，明星与电竞圈有了新的合作模式，鹿晗和黄子韬作为俱乐部合作伙伴开始了电竞的副业。赛会官员和电子竞技赛事之所以忠于明星的合作，在于明星可以带动电子竞技的影响力打破圈子，俱乐部合作明星的考量也是如此。</p><p >\n            <img width='100%' src='//dfzximg01.dftoutiao.com/minimodify/20210308/500x330_6045b4306dbe4.png' data-weight='500' data-width='500' data-height='330'></p><p >1月24日，杨幂、李现率领一队与KPL职业选手猫神、一诺在王者荣耀年度颁奖典礼明星表演比赛中PK。表演赛当晚，杨幂玩张飞，张飞趁势上微博搜索，为明星表演赛和年度王者荣耀颁奖典礼带来了一个话题上亿的讨论。如今，邀请明星与职业选手在重大电子竞技赛事中比拼已是家常便饭。比如，在英雄联盟成立9周年之际，周杰伦和王俊凯参赛后，相关话题依次出现在微博热搜上。电子竞技赛事邀请明星助阵，就像NBA邀请蔡徐坤代言一般，都对明星带来的价值和粉丝的转化感兴趣。</p><p >\n            <img width='100%' src='//dfzximg01.dftoutiao.com/minimodify/20210308/500x500_6045b435d800b.png' data-weight='500' data-width='500' data-height='500'></p><p >同时，这些明星背后的粉丝圈和电竞圈的主要粉丝都是年轻群体，他们的跨界本性也能带动电竞文化的粉丝冲破这堵墙。近年来，电子竞技圈明星的跨界互动形式已不仅仅是明星表演比赛。除了邀请明星在赛前祝福环节出现在网上，明星们有时还会被邀请参加重大比赛的解说。例如，罗云熙和张彬彬参加了英雄联盟S10全球总决赛SN对DWG决赛的解说。电子竞技项目在选择合作明星时往往有自己的考虑，除了明星的流量价值，明星本身是否与比赛有很强的相关性也是关键。</p><p >\n            <img width='100%' src='//dfzximg01.dftoutiao.com/minimodify/20210308/500x325_6045b435a8c77.png' data-weight='500' data-width='500' data-height='325'></p><p >如今，王者荣耀还开发了一种新的明星跨界电子竞技方式，即邀请易烊千玺、宋茜、吴亦凡、李现、杨幂五位明星担任游戏的品牌代言人。这意味着王者荣耀与这些明星合作很长一段时间。官方还表示，这五位明星将出现在许多活动，如王者的周年庆典，KPL比赛，可以说明星与电子竞技圈的合作在不断演进，这不仅体现在双方更深层的绑定上，也体现在更深层的合作模式上。</p>"
+    },
+    "error_code": 0
+}
+
+function indexData(num) {
+    let data = [];
+    let templetData = indexTemplet.result.data;
+    for (let i = 0; i < num; i++) {
+        let index = i % templetData.length;
+        let temp = Object.assign({}, templetData[index]);
+        temp.title = i + " " + temp.title;
+        temp.uniquekey = "uniquekey" + i + Date.now();
+        data.push(temp);
+    }
+    let temp = Object.assign({}, indexTemplet);
+    temp.result.data = data;
+    return temp;
+}
+
+function contentData(id) {
+    let temp = Object.assign({}, contentTemplet);
+    temp.result.uniquekey = id;
+    return temp;
+}
+export default {
+    indexData: indexData,
+    contentData: contentData
+}
